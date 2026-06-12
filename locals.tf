9210874af9cf1c7ac41ea14f738a280166397dcf
rajesh maddula <rajesh.maddula21@gmail.com>
2026-06-12 11:20:42 +0530

    update1

--- locals.tf
+++ locals.tf
@@ -1,14 +1,14 @@
 locals {
-  local.common_tags = {
-    Project = var.project
-    Environment = var.environment
-    Terraform = true
-  }
-  vpc_final_tags = merge(
-    local.common_tags,
-    {
-        Name = "${var.project}-${var.environment}"
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
     }
-    var.vpc_tags
-  )
+    vpc_final_tags = merge(
+        local.common_tags,
+        {
+            Name = "${var.project}-${var.environment}"
+        },
+        var.vpc_tags
+    )
 }
