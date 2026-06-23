33974d968dfc30bf03f7959f5d4cb58fc5be9c7c
rajesh maddula <rajesh.maddula21@gmail.com>
2026-06-23 07:52:48 +0530

    actions

--- main.tf
+++ main.tf
@@ -5,10 +5,8 @@ resource "aws_vpc" "main" {
   tags = local.vpc_final_tags
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "main"
-  }
+  tags = local.igw_final_tags
 }
