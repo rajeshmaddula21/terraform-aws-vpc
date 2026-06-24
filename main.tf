e77ebe74e5c59359e81940ff98c6e509d60d6ecd
rajesh maddula <rajesh.maddula21@gmail.com>
2026-06-24 07:23:52 +0530

    actions

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "public" {
   count = length(var.public_sunet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_sunet_cidrs[count.index]
-  availability_zones = local.azs_names[count.index]
+  availability_zone = local.azs_names[count.index]
   map_public_ip_on_launch = "true"
 
 
